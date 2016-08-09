28a85054f2b440af8c408b7dfc7e687de7b43aa8
Levi Thomason <me@levithomason.com>
2016-08-08 19:29:15 -0700

    test(Modal): test ModalDescription

--- test/specs/modules/Modal/Modal-test.js
+++ test/specs/modules/Modal/Modal-test.js
@@ -4,16 +4,18 @@ import Modal from 'src/modules/Modal/Modal'
 import ModalHeader from 'src/modules/Modal/ModalHeader'
 import ModalContent from 'src/modules/Modal/ModalContent'
 import ModalActions from 'src/modules/Modal/ModalActions'
+import ModalDescription from 'src/modules/Modal/ModalDescription'
 import * as common from 'test/specs/commonTests'
 
 describe('Modal', () => {
   common.isConformant(Modal)
   common.hasUIClassName(Modal)
   common.rendersChildren(Modal)
-  common.hasSubComponents(Modal, [ModalHeader, ModalContent, ModalActions])
+  common.hasSubComponents(Modal, [ModalHeader, ModalContent, ModalActions, ModalDescription])
 
   common.propKeyOnlyToClassName(Modal, 'active')
   common.propKeyOnlyToClassName(Modal, 'basic')
+  common.propKeyOnlyToClassName(Modal, 'fullscreen')
   common.propValueOnlyToClassName(Modal, 'size')
 
   it('renders a <div /> element', () => {
